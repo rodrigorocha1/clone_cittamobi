2fd2b5d789302ae3c4e3eb7e018b008a2e72a7f1
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-03-30 23:41:21 -0300

    Troca de modelagem

--- entidades/parada.py
+++ entidades/parada.py
@@ -2,7 +2,6 @@ from entidades.posicao import Posicao
 from entidades.linhas import Linha
 from entidades.posicao import Posicao
 from typing import Dict, List
-from entidades.onibus import Onibus
 
 
 class Parada:
@@ -11,15 +10,15 @@ class Parada:
         self.codigo_parada = json_parada['cp'] if json_parada.get('cp') is not None else None
         self.nome_parada = json_parada['np'] if json_parada.get('np') is not None else None
         self._posicao = Posicao(json_parada['py'], json_parada['px'])
-        self._onibus = []
+        self._linha = []
 
     @property
     def posicao(self) -> Posicao:
         return self._posicao
 
-    def adicionar_onibus(self, onibus: Onibus):
-        self._onibus.append(onibus)
+    def adicionar_linhas(self, linha: Linha):
+        self._onibus.append(linha)
 
     @property
     def mostrar_onibus(self):
-        return self._onibus
+        return self._linha
