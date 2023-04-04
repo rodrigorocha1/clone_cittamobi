615d09222d29cccb4079099fa54626b717396d81
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-04-04 20:13:02 -0300

    Refatoração da classe mapa

--- entidades/mapa.py
+++ entidades/mapa.py
@@ -4,15 +4,30 @@ import folium
 from services.posicao_veiculo_service import PosicaoVeiculo
 from services.parada_service import ParadaService
 from entidades.parada import Parada
-from typing import List
+from typing import List, Tuple
 import os
 
 
 class Mapa:
 
     def __init__(self):
-        self.__mapa = None
-        self.__camino = os.getcwd() + '\\mapas_html\\'
+        self._mapa = None
+        self._camino = os.getcwd() + '\\mapas_html\\'
+
+    def _criar_mapa(self, latitude: float, longitude: float):
+        return folium.Map(location=[latitude, longitude], zoom_start=14)
+
+    def _desenhar_linha(self, ponto_inicial: List, ponto_final: List, cor: str):
+        return folium.PolyLine(locations=[ponto_inicial, ponto_final], color=f'#{cor}').add_to(
+            self._mapa)
+
+    def _marcador_mapa(self, latitude: float, longitude: float, popup: str, icon: Tuple):
+        return folium.Marker([latitude, longitude],
+                             popup=popup,
+                             icon=folium.Icon(icon=icon[0], prefix=icon[1], color=icon[2])).add_to(self._mapa)
+
+    def _salvar_mapa(self, nome_arquivo_mapa: str):
+        self.__mapa.save(os.getcwd() + '\\mapas_html\\' + nome_arquivo_mapa)
 
     def criar_mapa_posicao(self, linhas: Linha):
         pv = PosicaoVeiculo()
@@ -24,37 +39,34 @@ class Mapa:
             titulo_html = f"""<h1>Mapa da linha {linha.terminal_principal, '-', linha.terminal_secundario}
                </h1><h1>Horário de Referência: {datetime.now().strftime('%HH:%MM:%SS')} </h1>"""
 
-            self.__mapa = folium.Map(location=[linha.trajeto.posicoes[len(linha.trajeto.posicoes) // 2].latitude,
-                                               linha.trajeto.posicoes[len(linha.trajeto.posicoes) // 2].longitude
-                                               ], zoom_start=14)
+            self._criar_mapa(linha.trajeto.posicoes[len(linha.trajeto.posicoes) // 2].latitude,
+                              linha.trajeto.posicoes[len(linha.trajeto.posicoes) // 2].longitude)
+
             for i in range(len(linha.trajeto.posicoes) - 1):
                 ponto_inicial = [linha.trajeto.posicoes[i].latitude, linha.trajeto.posicoes[i].longitude]
                 ponto_final = [linha.trajeto.posicoes[i + 1].latitude, linha.trajeto.posicoes[i + 1].longitude]
-                folium.PolyLine(locations=[ponto_inicial, ponto_final], color=f'#{linha.trajeto.cor}').add_to(
-                    self.__mapa)
+                self._desenhar_linha(ponto_inicial, ponto_final, linha.trajeto.cor).add_to(self._mapa)
 
             for posicao_veiculo in lista_posicoes_veiculos:
-                folium.Marker([posicao_veiculo.posicao.latitude, posicao_veiculo.posicao.longitude],
-                              popup=posicao_veiculo.prefixo,
-                              icon=folium.Icon(icon='bus', prefix='fa', color='blue')).add_to(self.__mapa)
+                icon = ('bus', 'fa', 'blue')
+                self._marcador_mapa(posicao_veiculo.posicao.latitude, posicao_veiculo.posicao.longitude,
+                                     posicao_veiculo.prefixo, icon).add_to(self._mapa)
 
             paradas = ps.buscar_paradas(linha.codigo_identificador)
+            icon = ('bus', 'fa', 'red')
             for parada in paradas:
-                folium.Marker([parada.posicao.latitude, parada.posicao.longitude], popup=parada.nome_parada,
-                              icon=folium.Icon(icon='bus', prefix='fa', color='red')).add_to(
-                    self.__mapa)
+                self._marcador_mapa(parada.posicao.latitude, parada.posicao.longitude, parada.nome_parada,
+                                     icon).add_to(self._mapa)
         # mapa_parada.get_root().html.add_child(folium.Element(titulo_html))
-        self.__mapa.save(os.getcwd() + '\\mapas_html\\mapa_linha.html')
+        self._salvar_mapa('mapa_linha.html')
 
     def criar_mapa_paradas(self, paradas: List[Parada]):
-        self.__mapa = folium.Map(location=[paradas[0].posicao.latitude, paradas[0].posicao.longitude], zoom_start=14)
+        self._mapa = self._criar_mapa(paradas[0].posicao.latitude, paradas[0].posicao.longitude)
+        icon = ('bus', 'fa', 'red')
         for parada in paradas:
-
-            folium.Marker([parada.posicao.latitude, parada.posicao.longitude],
-                          popup='a',
-                          icon=folium.Icon(icon='bus', prefix='fa', color='blue')).add_to(self.__mapa)
-        self.__mapa.save(os.getcwd() + '\\mapas_html\\mapa_posicoes_parada.html', )
+            self._marcador_mapa(parada.posicao.latitude, parada.posicao.longitude, 'a', icon).add_to(self._mapa)
+        self._salvar_mapa('mapa_posicoes_parada.html')
 
     def __del__(self):
-        for nome in os.listdir(self.__camino):
-            os.remove(self.__camino + nome)
+        for nome in os.listdir(self._camino):
+            os.remove(self._camino + nome)
