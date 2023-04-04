fc9466c54eed112d9cd1312dbd78470e92f37ccf
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-04-03 21:12:49 -0300

    trocando os atributos da classe

--- entidades/parada.py
+++ entidades/parada.py
@@ -4,11 +4,11 @@ from typing import Dict
 
 
 class Parada:
-    def __init__(self, json_parada: Dict):
-        self.endereco_localizacao = json_parada['ed'] if json_parada.get('ed') is not None else None
-        self.codigo_parada = json_parada['cp'] if json_parada.get('cp') is not None else None
-        self.nome_parada = json_parada['np'] if json_parada.get('np') is not None else None
-        self._posicao = Posicao(json_parada['py'], json_parada['px'])
+    def __init__(self, endereco_localizacao: str, codigo_parada: int, nome_parada: str, latitude: float, longitude: float):
+        self.endereco_localizacao = endereco_localizacao
+        self.codigo_parada = codigo_parada
+        self.nome_parada = nome_parada
+        self._posicao = Posicao(latitude,longitude)
         self._linha = []
 
     @property
