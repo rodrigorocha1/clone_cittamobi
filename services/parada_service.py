0f7cd570ef4852c2a7e92e1599c29057b1a22500
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-04-04 20:33:20 -0300

    remoção de método

--- services/parada_service.py
+++ services/parada_service.py
@@ -12,18 +12,17 @@ class ParadaService(ServiceSPTRANS):
     def __init__(self):
         super().__init__()
 
-    def buscar_paradas(self, id_linha: int) -> List[Parada]:
+    def buscar_paradas_por_linha(self, id_linha: int) -> List[Parada]:
         path = '/Parada/BuscarParadasPorLinha?codigoLinha=' + str(id_linha)
         if self._login():
             req = self._sptrans_api.requests_api(path, 'GET')
             lista_paradas = [Parada(*ValidadorJson(parada).validar_json_parada()) for parada in req]
             return lista_paradas
 
-    def buscar_todas_posicoes_paradas(self):
-        todas_paradas = pd.read_csv(os.getcwd() + '\\data\\raw\\stops.txt')
-        paradas = todas_paradas.apply(
-            lambda linha: Parada(codigo_parada=linha['stop_id'],
-                                 nome_parada=linha['stop_desc'],
-                                 endereco_localizacao=linha['stop_name'],
-                                 posicao=Posicao(linha['stop_lat'], linha['stop_lon'])), axis=1).tolist()
-        return paradas
+    def buscar_previsao_chegada_parada_endereco(self, endereco: str):
+        path = '/Previsao/Parada?codigoParada=' + endereco
+        if self._login():
+            req = self._sptrans_api.requests_api(path, 'GET')
+            return req
+
+
