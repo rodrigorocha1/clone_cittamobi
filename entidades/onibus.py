56332e68880852ebfa6836a6f9268b06b50085b9
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-04-04 22:07:48 -0300

    Adicão de horário previsto do ônibus

--- entidades/onibus.py
+++ entidades/onibus.py
@@ -2,8 +2,9 @@ from entidades.posicao import Posicao
 
 
 class Onibus:
-    def __init__(self, acessibiliade: bool, prefixo: str, posicao: Posicao):
+    def __init__(self, acessibiliade: bool, prefixo: str, posicao: Posicao, horario_previsto: str=None):
         self.acessibiliade = acessibiliade
+        self.horario_previsto = horario_previsto
         self.prefixo = prefixo
         self._posicao = posicao
 
