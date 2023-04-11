088bc54e6453d2e0fe7841037d69df50ea487133
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-04-11 20:16:02 -0300

    limpeza

---
+++ modulo_testecopy.py
@@ -0,0 +1,36 @@
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+from requests import get
+from functools import partial
+from os import getpid
+
+l_urls = ['https://google.com'] * 6
+
+# executor = ThreadPoolExecutor(max_workers=3)
+# result = executor.map(get, l_urls)
+# print(result)
+
+
+print('Threads')
+with ThreadPoolExecutor(max_workers=3) as executor:
+    """
+    executor.__enter__ -> self (ThreadPoolExecutor)
+    executor.__exit__ -> executor.shutdown(wait=True)
+    """
+    result = executor.map(get, l_urls)
+    print(result)
+
+    print(type(result))
+
+
+# print(list(result))
+
+# print('Processo')
+# with ProcessPoolExecutor() as executor:
+#     result = executor.map(partial(print, getpid(),))
+#     print(result)
+#     print(type(result))
+#     for r in result:
+#         print(r)
+
+# print(list(result))
