af3c205c14b09d549e5f80591cb6f092ce1a9100
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-04-10 22:19:02 -0300

    Teste de performance

--- modulo_teste.py
+++ modulo_teste.py
@@ -4,8 +4,8 @@ from time import time
 tempo_inicial = time()
 
 ps = ParadaService()
-paradas = ps.buscar_parada_previsao_endereco('Morumbi')
-for parada in paradas:
+previsao_paradas = ps.buscar_parada_previsao_endereco('USP')
+for parada in previsao_paradas:
     print('-primeiro for:', parada.nome_parada, '-', parada.codigo_parada)
     for linha in parada.mostrar_linha:
         print('--segundo for:', linha.codigo_identificador, '-',
@@ -14,7 +14,9 @@ for parada in paradas:
             print('---terceiro for:', onibus.prefixo)
             print('---Terceiro for:', onibus.horario_previsto)
             print('---Terceiro for: ', onibus.acessibiliade)
+            print()
     print()
 
 tempo_final = time()
-print(tempo_final - tempo_inicial, ' Segundos')
+duracao = round(tempo_final - tempo_inicial, 2)
+print(f'{duracao} Segundos')
