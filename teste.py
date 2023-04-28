74230c4074b7e8f2731067f2b3f6eb4513a045f2
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-04-27 23:05:58 -0300

    Teste para gerar layout em tabela

--- teste.py
+++ teste.py
@@ -1,15 +1,122 @@
 from dash import Dash, html
 import dash_bootstrap_components as dbc
+from typing import List
+from services.parada_service import ParadaService
+from entidades.parada import Parada
+from entidades.linhas import Linha
+from entidades.onibus import Onibus
+
+parada_service = ParadaService()
+paradas = parada_service.buscar_parada_endereco(endereco='USP')
+
+
+def gerar_linha_previsao_onibus(lista_onibus: List[Onibus]):
+    linha_onibus = html.Tr(
+        [
+            html.Td(
+                f'{onibus.numero_linha} - {onibus.letreiro_numerico}',
+                style={'font-size': '12px'}
+            ),
+            html.Td(
+                onibus.terminal_principal,
+                style={'font-size': '12px'}
+            ),
+            html.Td(
+                onibus.terminal_secundario,
+                style={'font-size': '12px'}
+            )
+        ] for onibus in lista_onibus
+    )
+    return linha_onibus
+
+
+def gerar_tabela(parada: Parada):
+    table_header_parada = [
+        html.Thead(
+            html.Tr(
+                [
+                    html.Th(
+                        f'Parada {parada.codigo_parada} - Endereço {parada.endereco_localizacao} - Nome {parada.nome_parada}',
+                        colSpan=3,
+                        style={
+                            'text-align': 'center',
+                            'font-size': '12px',
+                            'width': '50%',
+                        }
+                    )
+                ],
+            )
+        )
+    ]
+
+    previsoes_linha = parada_service.buscar_previsao_parada(
+        parada.codigo_parada)
+
+    table_header_previsao = [
+        html.Thead(
+            html.Tr(
+                [
+                    html.Th(
+                        f'{previsao_linha.codigo_identificador} -  {previsao_linha.letreiro_numerico} - '
+                        f'{previsao_linha.letreiro_numerico_segunda_parte} - {previsao_linha.terminal_principal} - {previsao_linha.terminal_secundario} ',
+                        colSpan=3,
+                        style={
+                            'text-align': 'center',
+                            'font-size': '12px',
+                            'width': '50%',
+                        }
+                    )
+                ],
+            )
+        ) for previsao_linha in previsoes_linha
+    ]
+
+
+def gerar_tabelas_cabecalho(lista_parada: List[Parada]):
+    cabecalho_hora_atualizacao = [
+        html.Thead(
+            html.Tr(
+                [
+                    html.Th(
+                        'Hora última atualização',
+                        colSpan=3,
+                        style={
+                            'text-align': 'center',
+                            'font-size': '12px',
+                            'width': '30%'
+                        }
+                    )
+                ],
+            )
+        )
+    ]
+
+    table_header = gerar_tabela([parada for parada in lista_parada])
+
+    table_body = [html.Tbody([row1])]
+
+    table = dbc.Table(cabecalho_hora_atualizacao + table_header + table_body,
+                      bordered=True,
+                      hover=True,
+                      responsive=True)
+    return table
+
+
+def gerar_previsao_linhas(linha: Linha):
+    pass
+
 
 table_header = [
     html.Thead(
         html.Tr(
             [
                 html.Th(
-                    "Hora última atualização",
+                    'Hora última atualização',
                     colSpan=3,
                     style={
-                        "text-align": "center"
+                        'text-align': 'center',
+                        'font-size': '12px',
+                        'width': '30%'
                     }
                 )
             ],
@@ -19,10 +126,12 @@ table_header = [
         html.Tr(
             [
                 html.Th(
-                    "Parada 450011848 - R TERESA MOUCO DE OLIVEIRA/ R AMANCIO PEDRO DE OLIVEIRA - PARADA HOSPITAL CAMPO LIMPO C/B",
+                    'Parada 450011848 - R TERESA MOUCO DE OLIVEIRA/ R AMANCIO PEDRO DE OLIVEIRA - PARADA HOSPITAL CAMPO LIMPO C/B',
                     colSpan=3,
                     style={
-                        "text-align": "center"
+                        'text-align': 'center',
+                        'font-size': '12px',
+                        'width': '50%',
                     }
                 )
             ],
@@ -32,9 +141,11 @@ table_header = [
         html.Tr(
             [
                 html.Th(
-                    "6450 10 TERM. BANDEIRA - TERM. CAPELINHA",
+                    '6450 10 TERM. BANDEIRA - TERM. CAPELINHA',
                     colSpan=3,
-                    style={"text-align": "center"}
+                    style={'text-align': 'center',
+                           'font-size': '12px',
+                           'width': '50%'}
                 )
             ],
         )
@@ -43,24 +154,31 @@ table_header = [
         html.Tr(
             [
                 html.Th(
-                    "Prefixo ônibus",
+                    'Prefixo ônibus',
+                    style={'font-size': '12px'}
                 ),
                 html.Th(
-                    "Previsão Chegada",
+                    'Previsão Chegada',
+                    style={'font-size': '12px'}
                 ),
                 html.Th(
-                    "Minutos Faltando",
+                    'Minutos Faltando',
+                    style={'font-size': '12px'}
                 ),
             ],
         )
     )
 ]
 
-row1 = html.Tr([html.Td("Arthur"), html.Td("Dent"),  html.Td("Dent")])
+row1 = html.Tr([html.Td('Arthur', style={'font-size': '12px'}), html.Td(
+    'Dent', style={'font-size': '12px'}),  html.Td('Dent', style={'font-size': '12px'})])
 
 table_body = [html.Tbody([row1])]
 
-table = dbc.Table(table_header + table_body, bordered=True)
+table = dbc.Table(table_header + table_body,
+                  bordered=True,
+                  hover=True,
+                  responsive=True)
 app = Dash(__name__,
            external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -69,23 +187,24 @@ app.layout = html.Div([
         [
             dbc.Row(
                 '     linha 1',
-                style={'color': 'black'}
+                style={'color': 'black',
+                       'font-size': '12px'}
             ),
             dbc.Row(
                 [
                     dbc.Col(
                         '1 coluna',
-                        style={'color': 'black'},
-                        md=8
+                        style={'color': 'black', 'font-size': '12px'},
+                        md=7
                     ),
                     dbc.Col(
                         [
                             html.Div(
                                 [
                                     dbc.Row(
-                                        table
+                                        gerar_tabelas_cabecalho(paradas)
                                     )
-                                    for i in range(1, 10)
+
                                 ],
                                 style={
                                     'color': 'black',
@@ -97,7 +216,7 @@ app.layout = html.Div([
                         ],
                         style={'height': '800px'},
 
-                        md=4
+                        md=5
                     )
                 ]
             )
